3ba831174fe0816ee24d3934ed79baf3873d88a4
Faith <fnbiketi@gmail.com>
2021-12-22 15:34:50 +0300

    testing my git

---
+++ Learn Path/index.js
@@ -0,0 +1,20 @@
+//alert("I'm javascript")
+
+
+
+
+//variables
+let admin;
+let nam='john';
+admin=nam
+
+//datatypes
+// let name="Ronana";
+// alert(`hello ${1}`);// hello 1
+// alert(`hello ${"name"}`);//hello name
+// alert(`hello ${name}`);//hello Ronana
+
+//interaction:Alert,Prompt,Confirm
+//A webapage that ask name and outputs it
+let jina=prompt('what is your name?');
+alert(`your name is ${jina}`);
